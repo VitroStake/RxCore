82a1a61c163b8d4293a620b3ec710cd53e0d2d70
vitro <vitrostake@gmail.com>
2022-03-27 11:46:18 +0900

    Change Stream accessibility to protected

---
+++ Assets/RxPort/Stream/Internal/StreamStore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+using System;
+
+namespace VitroStake.RxPort.Internal {
+  internal static class SubjectStore<TNotice, TPayload> where TNotice : Enum {
+    public static IObserver<TPayload> GetOrCreateObserver(TNotice notice) {
+      if (!_subjects.ContainsKey(notice))
+        _subjects[notice] = new Subject<TPayload>();
+
+      return _subjects[notice];
+    }
+
+    public static IObservable<TPayload> GetOrCreateObservable(TNotice notice) {
+      if (!_subjects.ContainsKey(notice))
+        _subjects[notice] = new Subject<TPayload>();
+
+      return _subjects[notice];
+    }
+
+    // _subjects doesn't need to be reset in Domain Reloading
+    private static Dictionary<TNotice, Subject<TPayload>> _subjects = new();
+  }
+}
--- Assets/RxPort/Stream/StreamPort.cs
+++ Assets/RxPort/Stream/StreamPort.cs
@@ -7,9 +7,36 @@ using UniRx;
 using UnityEngine.Assertions;
 
 namespace VitroStake.RxPort {
+  using Internal;
+
   public abstract class StreamPort {
-    public StreamPort() {
-      _disposables = new CompositeDisposable();
+    protected static class Stream {
+      public static void OnNext<TNotice>(TNotice notice) where TNotice : Enum {
+        var observer = SubjectStore<TNotice, Unit>.GetOrCreateObserver(notice);
+        observer.OnNext(Unit.Default);
+      }
+
+      public static IObservable<Unit> Of<TNotice>(TNotice notice) where TNotice : Enum {
+        var observable = SubjectStore<TNotice, Unit>.GetOrCreateObservable(notice);
+        Assert.IsNotNull(StreamDisposer.Instance);
+        return observable.TakeUntilDestroy(StreamDisposer.Instance);
+      }
+    }
+
+    protected static class Stream<TPayload> {
+      public static void OnNext<TNotice>(TNotice notice, TPayload payload) where TNotice : Enum {
+        if (payload == null)
+          throw new ArgumentNullException();
+
+        var observer = SubjectStore<TNotice, TPayload>.GetOrCreateObserver(notice);
+        observer.OnNext(payload);
+      }
+
+      public static IObservable<TPayload> Of<TNotice>(TNotice notice) where TNotice : Enum {
+        var observable = SubjectStore<TNotice, TPayload>.GetOrCreateObservable(notice);
+        Assert.IsNotNull(StreamDisposer.Instance);
+        return observable.TakeUntilDestroy(StreamDisposer.Instance);
+      }
     }
 
     public StreamPort Open() {
@@ -29,24 +56,27 @@ namespace VitroStake.RxPort {
       _opening = false;
     }
 
-    public bool IsOpen  => _opening;
-    public bool IsClose => !_opening;
-
-    protected abstract void OpenCore();
-    public abstract bool IsValid { get; }
-
     protected void Register(params IDisposable[] streams) {
       foreach (var stream in streams)
         _disposables.Add(stream);
     }
 
-    protected CompositeDisposable _disposables;
+    protected abstract void OpenCore();
+    public abstract bool IsValid { get; }
+
+    public bool IsOpen  => _opening;
+    public bool IsClose => !_opening;
+
+    public StreamPort() {
+      _disposables = new CompositeDisposable();
+    }
 
     // CompositeDisposable is not automatically disposed
     ~StreamPort() {
       _disposables.Dispose();
     }
 
+    protected CompositeDisposable _disposables;
     private bool _opening;
   }
 
