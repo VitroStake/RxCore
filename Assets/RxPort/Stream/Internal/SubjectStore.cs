809f43f55956b92b861a21fba40817f2f8e09bf0
vitro <vitrostake@gmail.com>
2022-04-05 14:13:05 +0900

    Fix

--- Assets/RxPort/Stream/Internal/SubjectStore.cs
+++ Assets/RxPort/Stream/Internal/SubjectStore.cs
@@ -7,11 +7,6 @@ using System;
 
 namespace VitroStake.RxPort.Internal {
   internal static class SubjectStore<TNotice, TPayload> where TNotice : Enum {
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    static void OnRuntimeInitialize() {
-      _subjects = new();
-    }
-
     public static IObserver<TPayload> GetOrCreateObserver(TNotice notice) {
       if (!_subjects.ContainsKey(notice))
         _subjects[notice] = new Subject<TPayload>();
@@ -26,6 +21,10 @@ namespace VitroStake.RxPort.Internal {
       return _subjects[notice];
     }
 
+    // RuntimeInitializeOnLoadMethod is not called in Generic classes,
+    // so they have no means of initializing static fields without domain reload.
+    // But in case of SubjectStore, _subjects doesn't need to be initialized other than capacity issue,
+    // because it doesn't have any states.
     private static Dictionary<TNotice, Subject<TPayload>> _subjects = new();
   }
 }
