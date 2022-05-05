5abecd6d865510e606da412c94d76c6546f8dd83
vitro <vitrostake@gmail.com>
2022-05-05 20:22:42 +0900

    Update

--- Assets/RxPort/Stream/Stream.cs
+++ Assets/RxPort/Stream/Stream.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 using System;
 using UniRx;
-using UniRx.Triggers;
 using UnityEngine.Assertions;
 
 namespace VitroStake.RxPort {
@@ -76,72 +75,5 @@ namespace VitroStake.RxPort {
 
       return new StreamSource<TStreamId, TNotice>(id, notice);
     }
-  }
-
-  public abstract class StreamPort {
-    protected static class Stream {
-      public static IObservable<Unit> Of<TStreamId, TNotice>(TStreamId id, TNotice notice)
-        where TStreamId : struct
-        where TNotice : Enum {
-
-        var observable = SubjectStore<TStreamId, TNotice, Unit>.GetOrCreateObservable(id, notice);
-        Assert.IsNotNull(StreamDisposer.Instance);
-        return observable.TakeUntilDestroy(StreamDisposer.Instance);
-      }
-    }
-
-    // In order to pay attention to the type of a payload, Stream<TPayload> is divided from Stream.
-    protected class Stream<TPayload> {
-      public static IObservable<TPayload> Of<TStreamId, TNotice>(TStreamId id, TNotice notice)
-        where TStreamId : struct
-        where TNotice : Enum {
-
-        var observable = SubjectStore<TStreamId, TNotice, TPayload>.GetOrCreateObservable(id, notice);
-        Assert.IsNotNull(StreamDisposer.Instance);
-        return observable.TakeUntilDestroy(StreamDisposer.Instance);
-      }
-    }
-
-    public void Open() {
-      Assert.IsFalse(_opening);
-      Assert.IsTrue(IsValid);
-
-      var streams = SubscribeStreams();
-      Register(streams);
-
-      _opening = true;
-
-      PortStore.UpdateOrAddPort(this);
-    }
-
-    public void Close() {
-      _disposables.Clear();
-      _opening = false;
-    }
-
-    protected void Register(params IDisposable[] streams) {
-      foreach (var stream in streams)
-        _disposables.Add(stream);
-    }
-
-    protected abstract IDisposable[] SubscribeStreams();
-    public abstract bool IsValid { get; }
-
-    public bool IsOpen  => _opening;
-    public bool IsClose => !_opening;
-
-    public StreamPort() {
-      Assert.IsNotNull(StreamDisposer.Instance);
-
-      StreamDisposer.Instance.OnDestroyAsObservable()
-        .Subscribe(_ =>
-        {
-          // You have to manually dispose a CompositeDisposable because it's not automatically disposed.
-          _disposables.Dispose();
-        });
-    }
-
-    protected CompositeDisposable _disposables = new();
-    private bool _opening;
   }
 }
